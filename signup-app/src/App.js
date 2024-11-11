6457fc68c224a4a9bd4b14b46ebd596257571f12
Anish Agarwal <121827018+docengaimiit@users.noreply.github.com>
2024-11-11 20:48:11 +0530

    Add files via upload

---
+++ signup-app/src/App.js
@@ -0,0 +1,13 @@
+// client/src/App.js
+import React from 'react';
+import SignupForm from './SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
